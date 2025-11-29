caee13ecbd78a5f9dd9d960252e17c67f7a88906
ssuarezp <ssuarezp@unal.edu.co>
2025-11-29 12:40:08 -0500

    última actualización

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -3,7 +3,6 @@
 
 
 import argparse
-import sys
 
 from homework.src._internals.read_all_lines import read_all_lines
 
@@ -24,6 +23,26 @@ def parse_args():
     return parsed_args.input, parsed_args.output
 
 
+def preprocess_lines(lines):
+    pass
+
+
+def split_into_words(preprocessed_lines):
+    pass
+
+
+def count_words(words):
+    pass
+
+
+def write_word_counts(output_folder, word_counts):
+    pass
+
+
 def main():
     input_folder, output_folder = parse_args()
     lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
