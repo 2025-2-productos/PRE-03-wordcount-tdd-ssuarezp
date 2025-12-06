4029e965625b8dfc6075b26431c3a78705ee4aee
ssuarezp <ssuarezp@unal.edu.co>
2025-12-06 07:50:46 -0500

    ADD: se agregan funciones

--- homework/src/_internals/tests/test_internals.py
+++ homework/src/_internals/tests/test_internals.py
@@ -2,14 +2,12 @@ import os
 import shutil
 import sys
 
-from ...wordcount import (
-    count_words,
-    parse_args,
-    preprocess_lines,
-    split_into_words,
-    write_word_counts,
-)
+from ...wordcount import parse_args
+from ..count_words import count_words
+from ..preprocess_lines import preprocess_lines
 from ..read_all_lines import read_all_lines
+from ..split_into_words import split_into_words
+from ..write_word_counts import write_word_counts
 
 
 def test_parse_args():
